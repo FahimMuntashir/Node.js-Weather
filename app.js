1718cc621037a86baf52cebd92359615e31b5fb0
FahimMuntashir <fahim.tech.me@gmail.com>
2020-06-07 01:00:41 +0600

    Https passing

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const https = require("https");
+
 const app = express();
 
-app.get('/', function(req,res){
+
+app.get("/", function(req,res){
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Bangladesh&units=metric&appid=304fa1cfd3e50e77d732349b3e590b58";
+
+    https.get(url, function(response){
+        console.log(response);
+        
+
+    });
     res.send("hello world");
 });
 
