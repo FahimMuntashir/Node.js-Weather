d6001aed3d593e959b1fc06952ffa7a3d881f11d
FahimMuntashir <fahim.tech.me@gmail.com>
2020-06-08 01:04:37 +0600

    parse JSON

--- app.js
+++ app.js
@@ -8,7 +8,20 @@ app.get("/", function(req,res){
     const url = "https://api.openweathermap.org/data/2.5/weather?q=Bangladesh&units=metric&appid=304fa1cfd3e50e77d732349b3e590b58";
 
     https.get(url, function(response){
-        console.log(response);
+        console.log(response.statusCode);
+
+        response.on("data", function(data){
+
+           const dataParse= JSON.parse(data);
+
+           const temp = dataParse.main.temp;
+           console.log(temp);
+           const temp2 = dataParse.weather[0].description;
+           console.log(temp2);
+           
+
+           
+        })
         
 
     });
