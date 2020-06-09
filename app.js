e0fb0b57da0074497ab3b0270848b3d0421215eb
FahimMuntashir <fahim.tech.me@gmail.com>
2020-06-09 16:56:19 +0600

    bodyParser

--- app.js
+++ app.js
@@ -1,32 +1,50 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:true}));
+
 
 app.get("/", function(req,res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Bangladesh&units=metric&appid=304fa1cfd3e50e77d732349b3e590b58";
+    res.sendFile(__dirname + "/index.html");
 
-    https.get(url, function(response){
-        console.log(response.statusCode);
-
-        response.on("data", function(data){
-
-           const dataParse= JSON.parse(data);
-
-           const temp = dataParse.main.temp;
-           console.log(temp);
-           const temp2 = dataParse.weather[0].description;
-           console.log(temp2);
-           
-
-           
-        })
-        
-
-    });
-    res.send("hello world");
+   
 });
+app.post("/" , function(req, res){
+    
+    const query = req.body.cityName;
+const apikey ="304fa1cfd3e50e77d732349b3e590b58";
+const unit = "metric"; 
+const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=" + unit + "&appid=" + apikey ;
+
+https.get(url, function(response){
+    console.log(response.statusCode);
+
+    response.on("data", function(data){
+
+       const dataParse= JSON.parse(data);
+
+       const temp = dataParse.main.temp;
+       console.log( );
+       const temp2 = dataParse.weather[0].description;
+       console.log(temp2);
+       const icon = dataParse.weather[0].icon;
+       const imageURL = "http://openweathermap.org/img/wn/"+icon+"@2x.png "
+
+       res.write("<h1>the temperature of " + query + " is " +temp + " degree celcius</h1>");
+       res.write("<img src = "+ imageURL+">");
+       res.send();
+
+       
+    })
+    
+
+});
+})
+
+
 
 
 app.listen(3000, function(req,res){
